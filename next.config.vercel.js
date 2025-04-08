4df9aee8efed21d708c7e15011fa1039e6f51791
Prankur Shukla Reso <prankur.s@resolink.in>
2025-04-08 16:50:56 +0530

    ready to deploy

---
+++ next.config.vercel.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['github.com']
+  },
+  transpilePackages: ['lucide-react'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './src'),
+    };
+    return config;
+  },
+  experimental: {
+    esmExternals: true
+  }
+};
+
+module.exports = nextConfig; 
